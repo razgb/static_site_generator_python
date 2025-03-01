69985d4bd02a3e50298569cb7fa1bd9d38612fef
razgb <hamaraz2003@gmail.com>
2025-03-01 15:46:48 +0300

    3 more unit tests to textnode

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -9,12 +9,26 @@ class TestTextNode(unittest.TestCase):
 
     def test_not_eq(self):
         node = TextNode("testing textnode", TextType.BOLD)
-        node2 = TextNode("different content", TextType.ITALIC)
+        node2 = TextNode("different content", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_not_eq2(self):
+        node = TextNode("testing textnode", TextType.BOLD)
+        node2 = TextNode("testing textnode", TextType.ITALIC)
         self.assertNotEqual(node, node2)
 
     def test_none_as_url(self):
         node = TextNode("testing textnode", TextType.BOLD)
         self.assertEqual(node.url, None)
+
+    def test_url_eq(self):
+        node = TextNode("testing textnode", TextType.ITALIC, "http://example.com")
+        node2 = TextNode("testing textnode", TextType.ITALIC, "http://example.com")
+        self.assertEqual(node, node2)
+
+    def test_repr(self):
+        node = TextNode("testing textnode", TextType.ITALIC)
+        self.assertEqual("TextNode(testing textnode, italic, None)", repr(node))
 
 if __name__ == "__main__":
     unittest.main()
